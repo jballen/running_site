e483fb9821dea51196433191b7410f6a4b7e6666
Brock Allen <jballen92@gmail.com>
2013-12-23 13:13:54 -0500

    Made a basic user model with secure passwords

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,9 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+    it "should have the content 'LogAJog'" do
+      visit root_path
+      expect(page).to have_content('LogAJog')
     end
   end
 end
