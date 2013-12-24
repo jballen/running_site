21f5f74039c05b0de119799b83c8de874d9ec093
Brock Allen <jballen92@gmail.com>
2013-12-24 16:54:17 -0500

    Finished user editing and updating. The backend for deleting users was added bus it not currently implemented.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,8 +3,24 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
 
-   describe "signup page" do
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+  end
+
+  describe "signup page" do
 
     before { visit signup_path }
 
