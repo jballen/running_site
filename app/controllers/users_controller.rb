0cb99a55e25f207a3e9117e80158410400de4377
Brock Allen <jballen92@gmail.com>
2013-12-24 09:27:43 -0500

    Finished implementing sign in and sign out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,27 @@
 class UsersController < ApplicationController
+  
   def show
   	@user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to LogAJog!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url 
   end
 
   private
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,31 @@
+module SessionsHelper
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
+  end
+
+  def sign_out
+    # Create a new remember_token to invalidate the old one
+    current_user.update_attribute(:remember_token, 
+                                  User.encrypt(User.new_remember_token))
+    # Delete the old remember_token from cookies
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    # The ||= operator only performs the lookup if current_user is undefined
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+end
